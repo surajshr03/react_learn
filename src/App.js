d46eb256fef48117c56f4ce777f66ab1b3f4a155
Suraj Shrestha <suraj.shr03@gmail.com>
2023-12-18 14:39:30 +0545

    inintial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let name = `suraj`;
+  let age = `22`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div style={{textAlign:'center'}}>
+      <p>My name is {name} and my age is {age}.</p>
     </div>
+    
   );
 }
 
